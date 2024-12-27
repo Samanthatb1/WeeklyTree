8782e9f6acabb5f9634db6ca56af0e9dce91f175
Samantha Grieco <samtb1@outlook.com>
2024-12-27 16:01:09 -0500

    debugging 3

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,3 +1,4 @@
+
 import { Route, Routes } from "react-router-dom";
 import Layout from "./components/Layout/Layout"
 import Landing from "./components/Landing/Landing"
@@ -7,14 +8,24 @@ import Success from "./components/Success/Success";
 function App() {
   return (
     <Routes>
-      {/* Layout renders the children thats passed in*/}
-      <Route path="/" element={<Layout />}>
-        <Route index element={<Landing />} />
-        <Route path="unsubscribe" element={<Unsubscribe />} />
-        <Route path="success" element={<Success />} />
-      </Route>
+      <Route path="/" element={<h1>Landing Page</h1>} />
+      <Route path="/unsubscribe" element={<h1>Unsubscribe Page</h1>} />
     </Routes>
   );
 }
+
+
+// function App() {
+//   return (
+//     <Routes>
+//       {/* Layout renders the children thats passed in*/}
+//       <Route path="/" element={<Layout />}>
+//         <Route index element={<Landing />} />
+//         <Route path="unsubscribe" element={<Unsubscribe />} />
+//         <Route path="success" element={<Success />} />
+//       </Route>
+//     </Routes>
+//   );
+// }
 
 export default App;
